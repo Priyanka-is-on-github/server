3fc78e0c2430b6312554475ce537fb2d18d7e33f
Priyanka Singh <priyankasingh8885@gmail.com>
2024-10-20 00:21:16 +0530

    dbmigration Fix

--- dbmigration.ts
+++ dbmigration.ts
@@ -1,30 +1,24 @@
-const pool = require("../db");
+const pool = require("./db");
 
 
 async function table(){
 
-    await pool.query(`CREATE TABLE attachment(
-        id BIGSERIAL NOT NULL,
-        name VARCHAR(255),
-        url TEXT,
-        courseid INT NOT NULL,
-        createdat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-        updatedat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+   
+await pool.query(`CREATE TABLE course(
+    id BIGSERIAL PRIMARY KEY,
+    teacherid VARCHAR(255) ,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+     imageurl TEXT,
+     price NUMERIC(10,2),
+     ispublished BOOLEAN DEFAULT false,
+    categoryid VARCHAR(255),
+    progress_percentage INT,
     
-         CONSTRAINT fk_categorymodel
-            FOREIGN KEY(courseid)
-                REFERENCES course(id)
-    
-    
-            )`)
-
-            await pool.query(`CREATE TABLE category(
-    id BIGSERIAL NOT NULL, 
-    name VARCHAR(255)
-
-
-    
-);`)
+    createdat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updatedat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+     
+    );`)
 
 await pool.query(`CREATE TABLE  IF NOT EXISTS chapters( 
     id BIGSERIAL NOT NULL PRIMARY KEY,
@@ -58,36 +52,46 @@ await pool.query(`CREATE TABLE  IF NOT EXISTS chapters(
                 REFERENCES chapters(id) ON DELETE CASCADE
     );
 `)
-    
-await pool.query(`    CREATE TABLE IF NOT EXISTS purchase(
-        id BIGSERIAL PRIMARY KEY,
-        userid INT NOT NULL,
 
-        courseid INT NOT NULL,
-        createdat TIMESTAMPTZ DEFAULT NOW(),
-            updatedat TIMESTAMPTZ DEFAULT NOW(),
-            CONSTRAINT fk_purchase
-            FOREIGN KEY(courseid)
-                REFERENCES course(id) ON DELETE CASCADE
-    );`)
 
   
 
 
-await pool.query(`CREATE TABLE Course(
-id BIGSERIAL PRIMARY KEY,
-teacherid VARCHAR(255) ,
-title VARCHAR(255) NOT NULL,
-description TEXT,
- imageurl TEXT,
- price NUMERIC(10,2),
- ispublished BOOLEAN DEFAULT false,
-categoryid VARCHAR(255),
-progress_percentage INT,
 
-createdat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updatedat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
- 
+    
+await pool.query(`    CREATE TABLE IF NOT EXISTS purchase(
+    id BIGSERIAL PRIMARY KEY,
+    userid INT NOT NULL,
+
+    courseid INT NOT NULL,
+    createdat TIMESTAMPTZ DEFAULT NOW(),
+        updatedat TIMESTAMPTZ DEFAULT NOW(),
+        CONSTRAINT fk_purchase
+        FOREIGN KEY(courseid)
+            REFERENCES course(id) ON DELETE CASCADE
+);`)
+
+await pool.query(`CREATE TABLE attachment(
+    id BIGSERIAL NOT NULL,
+    name VARCHAR(255),
+    url TEXT,
+    courseid INT NOT NULL,
+    createdat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updatedat TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+     CONSTRAINT fk_categorymodel
+        FOREIGN KEY(courseid)
+            REFERENCES course(id)
+
+
+        )`)
+
+        await pool.query(`CREATE TABLE category(
+id BIGSERIAL NOT NULL, 
+name VARCHAR(255)
+
+
+
 );`)
 
 await pool.query(`    CREATE TABLE IF NOT EXISTS userprogress (
